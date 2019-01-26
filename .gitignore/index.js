5a6e2f69bbc980fdf1a97bf450ce1401105ea534
NeoFR123 <46282675+NeoFR123@users.noreply.github.com>
2019-01-26 21:31:10 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -43,30 +43,3 @@ Client.on("message", async (message) => {
 });
 
 Client.login(process.env.TOKEN);
-
-exports.run = async (bot, message, args, ops) => {
-
-	if (!message.member.roles.find("name", "@Responsable Formulaire")) { //Whatever role you want, I pick @everyone because everyone can use this command
-		message.channel.send('Invalid permissions.');
-		return;
-	}
-    
-    // Check for input
-    if (!args[0]) return message.channel.send('Proper usage: a.poll <question>');
-    
-    // Create Embed
-    const embed = new Discord.RichEmbed()
-        .setColor("#ffffff") //To change color do .setcolor("#fffff")
-        .setFooter('React to Vote.')
-        .setDescription(args.join(' '))
-        .setTitle(`Poll Created By ${message.author.username}`);
-        
-    let msg = await message.channel.send(embed)
-        .then(function (msg) {
-            msg.react("❎");
-            msg.react("✅"); // You can only add two reacts
-            message.delete({timeout: 1000});
-            }).catch(function(error) {
-            console.log(error);
-        });
-};
